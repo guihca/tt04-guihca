075b51b6ccec911e7c7e12756f7da6259bece53c
guihca <guihca@hotmail.com>
2023-09-01 18:29:04 -0300

    Update config.tcl

--- src/config.tcl
+++ src/config.tcl
@@ -58,4 +58,5 @@ set ::env(CLOCK_PORT) {clk}
 set ::env(DESIGN_IS_CORE) 0
 set ::env(RT_MAX_LAYER) {met4}
 
-set ::env(RUN_LINTER) 0
+set ::env(LINTER_INCLUDE_PDK_MODELS) 1
+#set ::env(RUN_LINTER) 0
